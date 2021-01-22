e7a0780c80bbbfd37c277bc5bdf878fbd746783b
bhuwan1998 <rfperfect785@gmail.com>
2021-01-22 18:35:35 +1100

    init

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -7,10 +7,10 @@ export const homeObjOne = {
     headLine: 'Headline', 
     description: 'Brief Description of services provided by the business.',  
     imgStart: false, 
-    img: require('../../images/const.svg'),
+    img: require('../images/const.svg'),
     buttonLabel: 'Services',
     alt: 'Construction', 
     dark: true, 
     primary: true, 
-    darkText: true
+    darkText: true,
 };
